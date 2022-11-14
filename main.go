85fd60c73d95b8a4d28e19f13299b57650b0586e
Brandon Chua <mrbrandonchua@gmail.com>
2022-11-15 02:15:37 +0800

    Added POW

--- main.go
+++ main.go
@@ -1,60 +1,28 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+	"strconv"
+
+	"github.com/brandonchua/go-blockchain/blockchain"
 )
 
-// Simplify Blockchain structure for now
-type BlockChain struct {
-	blocks []*Block
-}
-
-type Block struct {
-	Hash		[]byte
-	Data		[]byte
-	PrevHash	[]byte
-}
-
-// Simplify hashing function for now
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
-	return block
-}
-
-
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
-}
-
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
-}
-
 func main() {
-	chain := InitBlockChain()
+	chain := blockchain.InitBlockChain()
 
-	chain.AddBlock("1st Block after Genesis Block")
-	chain.AddBlock("2nd Block after Genesis Block")
-	chain.AddBlock("3rd Block after Genesis Block")
+	chain.AddBlock("First Block after Genesis")
+	chain.AddBlock("Second Block after Genesis")
+	chain.AddBlock("Third Block after Genesis")
 
-	for _, block := range chain.blocks {
+	for _, block := range chain.Blocks {
+
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
 	}
 }
